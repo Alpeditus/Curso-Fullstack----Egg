fecda40fa47b645a39926cbdc484539772646d7f
Cristian Castillo <crcastillo@ledesma.com.ar>
2023-08-22 00:30:03 -0300

    javascript

--- Guía 18/Facu/script.js
+++
@@ -1,4 +0,0 @@
-var num = parseInt(prompt("Ingrese un número."));
-var num2 = parseInt(prompt("Ingrese otro número."));
-alert(`La suma de sus números son ${num+num2}, hasta luego.`)
-document.write(`Su resultado fue ${num+num2}.`)
