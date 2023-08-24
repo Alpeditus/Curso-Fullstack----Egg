83e5bcfcaa11d9e8288ff77ccbc74c5afd3fe07b
Cristian Castillo <crcastillo@ledesma.com.ar>
2023-08-24 00:48:27 -0300

    Javascript

--- Guía 18/js/Ejercicio24.js
+++ Guía 18/js/Ejercicio24.js
@@ -1,12 +1,28 @@
 /* Escribir un programa de JavaScript que a través de un formulario calcule el radio de un
-circulo y lo muestre en el HTML. */ 
+circulo y lo muestre en el HTML. */
 
-document.getElementById("boton").addEventListener("click",()=>{
-    let radio = parseInt(document.getElementById("radio").value)
-    let perimetro = 2 * radio * Math.PI;
-    let area = Math.PI * Math.pow(radio,2);
+document.getElementById("boton").addEventListener("click", () => {
+  let radio = parseInt(document.getElementById("radio").value);
+  //Para identificar la figura se usa el querySelector.
+  let figura = document.querySelector('input[name="figura"]:checked').value;
 
-    document.getElementById("respuesta").innerHTML = 
-    `El perímetro del círculo es ${perimetro.toFixed(2)}. <br>
-     El área del círculo es ${area.toFixed(2)}.`
-})
+  //let figura = parseInt(document.getElementById("figura").value);
+  let perimetro; // = 2 * radio * Math.PI;
+  let area; // = Math.PI * Math.pow(radio,2);
+
+  switch (figura) {
+    case "1":
+      perimetro = 2 * radio * Math.PI;
+      area = Math.PI * Math.pow(radio, 2);
+      break;
+    case "2":
+      perimetro = 4 * radio;
+      area = Math.pow(radio, 2);
+      break;
+  }
+
+  document.getElementById(
+    "respuesta"
+  ).innerHTML = `El perímetro del círculo es ${perimetro.toFixed(2)}. <br>
+     El área del círculo es ${area.toFixed(2)}.`;
+});
---
+++ Guía 18/js/Ejercicio25.js
@@ -0,0 +1,50 @@
+/* Escriba una función de JavaScript para obtener los valores de Nombre y Apellido del
+siguiente formulario.
+<!DOCTYPE html>
+<html><head>
+<meta charset=utf-8 />
+<title>Obtener nombre y apellido de form </title>
+</head><body>
+<form id="form1" onsubmit="getFormValores()">
+Nombre: <input type="text" name="nombre" value="David"><br>
+Apellido: <input type="text" name="apellido" value="Beckham"><br>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html> */
+
+//Se usa &lt; y &gt; en lugar de < y > para que el innerHTML no interprete las etiquetas.
+//<pre></pre> Interpreta automáticamente los saltos de línea.
+document.getElementById("consigna").innerHTML = `<pre>Escriba una función de JavaScript para obtener los valores de Nombre y Apellido del
+siguiente formulario.
+&lt;!DOCTYPE html&gt;
+&lt;html&gt;&lt;head&gt;
+&lt;meta charset=utf-8 /&gt;
+&lt;title&gt;Obtener nombre y apellido de form &lt;/title&gt;
+&lt;/head&gt;&lt;body&gt;
+&lt;form id="form1" onsubmit="getFormValores()"&gt;
+Nombre: &lt;input type="text" name="nombre" value="David"&gt;&lt;br&gt;
+Apellido: &lt;input type="text" name="apellido" value="Beckham"&gt;&lt;br&gt;
+&lt;input type="submit" value="Submit"&gt;
+&lt;/form&gt;
+&lt;/body&gt;&lt;/html&gt;</pre>`;
+
+// script.js
+
+function getFormValores() {
+    // Obtener los elementos de entrada
+    var nombreInput = document.getElementsByName('nombre')[0];
+    var apellidoInput = document.getElementsByName('apellido')[0];
+  
+    // Obtener los valores de los elementos de entrada
+    var nombre = nombreInput.value;
+    var apellido = apellidoInput.value;
+  
+    // Imprimir los valores en la consola (puedes ajustar esto según tus necesidades)
+    console.log('Nombre:', nombre);
+    console.log('Apellido:', apellido);
+  
+    // Evitar que el formulario se envíe realmente
+    return false;
+  }
+  
